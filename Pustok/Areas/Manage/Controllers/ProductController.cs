90f5bb2c54d72f7dbe463eb0df877dd7abd5788b
[Chinara0303] <chinaraei@code.edu.az>
2022-04-07 21:24:32 +0400

    Add project files.

---
+++ Pustok/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Product> products = await _context.Products.OrderByDescending(p=>p.Id).Take(5).ToListAsync();
+
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
